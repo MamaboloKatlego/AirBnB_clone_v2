b00b86ce804ed3798a5f047d5987c9ef653a1503
MamaboloKatlego <132917857+MamaboloKatlego@users.noreply.github.com>
2024-03-07 23:52:26 +0200

    Update 3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,61 +1,74 @@
 #!/usr/bin/python3
 """
-Module Docs
+Write a Fabric script that generates a .tgz
 """
-from os.path import exists, isdir
+from fabric.api import (local)
+import tarfile
 from datetime import datetime
-from fabric.api import put, local, env, run
-env.hosts = ["54.237.61.71", "54.146.64.127"]
+
+import os
+# First we import the Fabric api
+from fabric.api import *
+
+env.hosts = ['35.175.135.174', '18.234.169.222']
 
 
 def do_pack():
     """
-    Function Docs
+    used to create a tar file.
     """
     try:
-        if isdir("versions") is False:
-            local("mkdir versions")
-        file = "versions/web_static_{}.tgz".format(
-                    datetime.now().strftime("%Y%m%d%H%M%S")
-                )
-        local("tar -cvzf {} web_static".format(file))
-        return file
+        folder = "web_static"
+        tyme = datetime.now().strftime("%Y%m%d%H%M%S")
+        tar_name = "versions/web_static_{}.tgz".format(tyme)
+        local('mkdir -p versions')
+        local('tar -cvzf {} web_static'.format(tar_name))
+        return (tar_name)
     except Exception:
-        return None
+        return (None)
 
 
 def do_deploy(archive_path):
     """
-    Function Docs
+    Write a Fabric script (based on the file
+    1-pack_web_static.py) that distributes an archive
+     to your web servers, using the function
     """
-    if exists(archive_path) is False:
-        return False
-    try:
-        file = archive_path.split("/")[-1]
-        ext = file.split(".")[0]
-        static_dir = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        commands = [
-                "mkdir -p {}{}/".format(static_dir, ext),
-                "tar -xzf /tmp/{} -C {}{}/".format(file, static_dir, ext),
-                "rm /tmp/{}".format(file),
-                "mv {0}{1}/web_static/* {0}{1}/".format(static_dir, ext),
-                "rm -rf {}{}/web_static".format(static_dir, ext),
-                "rm -rf /data/web_static/current".format(),
-                "ln -s {}{}/ /data/web_static/current".format(static_dir, ext),
-                ]
-        for command in commands:
-            run(command)
-        return True
-    except Exception:
-        return False
+    if (os.path.exists(archive_path)):
+        try:
+            f_name = archive_path.split('/')[-1]
+            name = f_name.split('.')[0]
+            store = '/data/web_static/releases/{}'.format(name)
+            rf_path = '/tmp/{}'.format(f_name)
+            l1 = '/data/web_static/current/web_static/*'
+            l2 = '/data/web_static/current'
+            run('mkdir -p /tmp')
+            run('mkdir -p {}'.format(store))
+            put(archive_path, '/tmp')
+            run('tar -xzvf {} -C {}'.format(rf_path, store))
+            run('rm -rf /tmp/{}'.format(f_name))
+            run('rm -rf /data/web_static/current')
+            run('ln -s {} /data/web_static/current'.format(store))
+            run('rsync -av --remove-source-files {}/web_static/* {}'
+                .format(store, store))
+            run('rm -rf {}/web_static'.format(store))
+            return (True)
+        except Exception:
+            return(False)
+    else:
+        return (False)
 
 
 def deploy():
     """
-    Function Docs
+    Write a Fabric script (based on the file
+     2-do_deploy_web_static.py) that creates and
+    distributes an archive to your web servers,
+    using the function deploy.
     """
-    archive_path = do_pack()
-    if archive_path is None:
-        return False
-    return do_deploy(archive_path)
+    a_path = do_pack()
+    if (a_path is None):
+        return (None)
+    else:
+        res = do_deploy(a_path)
+        return (res)
