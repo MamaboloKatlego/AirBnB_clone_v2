af0b8e39da7dbf4679cea88849c199ad5dc8977f
MamaboloKatlego <132917857+MamaboloKatlego@users.noreply.github.com>
2024-03-07 23:47:29 +0200

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,50 +1,49 @@
 #!/usr/bin/python3
+""" module doc
 """
-Module doc
-"""
-from os.path import exists, basename, splitext
+from fabric.api import task, local, env, put, run
 from datetime import datetime
-from fabric.api import env, task, put, local, run
-env.use_ssh_config = True
-env.hosts = ["54.237.61.71", "54.146.64.127"]
+import os
+
+env.hosts = ['18.207.1.87', '52.206.189.175']
 
 
+@task
 def do_pack():
+    """ method doc
+        sudo fab -f 1-pack_web_static.py do_pack
     """
-    Function Docs
-    """
-    file = "versions/web_static_{}.tgz".format(
-            datetime.now().strftime('%Y%m%d%H%M%S')
-            )
-    print("Packing web_static to {file}".format(file))
-    if local("mkdir -p versions && tar -cvzf {file} web_static".format(file)).succeeded:
-        return file
+    formatted_dt = datetime.now().strftime('%Y%m%d%H%M%S')
+    mkdir = "mkdir -p versions"
+    path = "versions/web_static_{}.tgz".format(formatted_dt)
+    print("Packing web_static to {}".format(path))
+    if local("{} && tar -cvzf {} web_static".format(mkdir, path)).succeeded:
+        return path
     return None
 
 
+@task
 def do_deploy(archive_path):
-    """
-    Function Docs
+    """ method doc
+        fab -f 2-do_deploy_web_static.py do_deploy:
+        archive_path=versions/web_static_20231004201306.tgz
+        -i ~/.ssh/id_rsa -u ubuntu
     """
     try:
-        if not exists(archive_path):
+        if not os.path.exists(archive_path):
             return False
-        ext = basename(archive_path)
-        no_ext, ext = splitext(ext)
-        web_static_dir = "/data/web_static/releases/"
+        fn_with_ext = os.path.basename(archive_path)
+        fn_no_ext, ext = os.path.splitext(fn_with_ext)
+        dpath = "/data/web_static/releases/"
         put(archive_path, "/tmp/")
-        commands = [
-                "rm -rf {}{}/".format(web_static_dir, no_ext),
-                "mkdir -p {}{}/".format(web_static_dir, no_ext),
-                "tar -xzf /tmp/{} -C {}{}/".format(ext, web_static_dir, no_ext),
-                "rm /tmp/{}".format(ext),
-                "mv {0}{1}/web_static/* {0}{1}/".format(web_static_dir, no_ext),
-                "rm -rf {}{}/web_static".format(web_static_dir, no_ext),
-                "rm -rf /data/web_static/current",
-                "ln -s {}{}/ /data/web_static/current".format(web_static_dir, no_ext),
-                ]
-        for command in commands:
-            run(command)
+        run("rm -rf {}{}/".format(dpath, fn_no_ext))
+        run("mkdir -p {}{}/".format(dpath, fn_no_ext))
+        run("tar -xzf /tmp/{} -C {}{}/".format(fn_with_ext, dpath, fn_no_ext))
+        run("rm /tmp/{}".format(fn_with_ext))
+        run("mv {0}{1}/web_static/* {0}{1}/".format(dpath, fn_no_ext))
+        run("rm -rf {}{}/web_static".format(dpath, fn_no_ext))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}{}/ /data/web_static/current".format(dpath, fn_no_ext))
         print("New version deployed!")
         return True
     except Exception:
