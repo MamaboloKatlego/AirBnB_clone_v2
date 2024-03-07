8f5d131517da9def76577ee1c9359b38643dd933
MamaboloKatlego <132917857+MamaboloKatlego@users.noreply.github.com>
2024-03-07 23:50:06 +0200

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,50 +1,59 @@
 #!/usr/bin/python3
-""" module doc
 """
-from fabric.api import task, local, env, put, run
-from datetime import datetime
+Script used to deploy an archive.
+"""
 import os
+from fabric.api import *
 
-env.hosts = ['18.207.1.87', '52.206.189.175']
-
-
-@task
-def do_pack():
-    """ method doc
-        sudo fab -f 1-pack_web_static.py do_pack
-    """
-    formatted_dt = datetime.now().strftime('%Y%m%d%H%M%S')
-    mkdir = "mkdir -p versions"
-    path = "versions/web_static_{}.tgz".format(formatted_dt)
-    print("Packing web_static to {}".format(path))
-    if local("{} && tar -cvzf {} web_static".format(mkdir, path)).succeeded:
-        return path
-    return None
+env.hosts = ['35.175.135.174', '18.234.169.222']
 
 
-@task
 def do_deploy(archive_path):
-    """ method doc
-        fab -f 2-do_deploy_web_static.py do_deploy:
-        archive_path=versions/web_static_20231004201306.tgz
-        -i ~/.ssh/id_rsa -u ubuntu
     """
+    Distributes an archive to web servers.
+
+    :param archive_path: Path to the archive file.
+    :return: True if all operations are done correctly, False otherwise.
+    """
+    if not os.path.exists(archive_path):
+        print(f"Error: Archive not found at {archive_path}")
+        return False
+
     try:
-        if not os.path.exists(archive_path):
-            return False
-        fn_with_ext = os.path.basename(archive_path)
-        fn_no_ext, ext = os.path.splitext(fn_with_ext)
-        dpath = "/data/web_static/releases/"
-        put(archive_path, "/tmp/")
-        run("rm -rf {}{}/".format(dpath, fn_no_ext))
-        run("mkdir -p {}{}/".format(dpath, fn_no_ext))
-        run("tar -xzf /tmp/{} -C {}{}/".format(fn_with_ext, dpath, fn_no_ext))
-        run("rm /tmp/{}".format(fn_with_ext))
-        run("mv {0}{1}/web_static/* {0}{1}/".format(dpath, fn_no_ext))
-        run("rm -rf {}{}/web_static".format(dpath, fn_no_ext))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {}{}/ /data/web_static/current".format(dpath, fn_no_ext))
-        print("New version deployed!")
+        # Extract necessary information from the archive_path
+        archive_name = os.path.basename(archive_path)
+        archive_no_ext = os.path.splitext(archive_name)[0]
+
+        # Define paths for deployment
+        store_path = '/data/web_static/releases/{}'.format(archive_no_ext)
+        remote_archive_path = '/tmp/{}'.format(archive_name)
+
+        # Create necessary directories
+        run('mkdir -p {}'.format(store_path))
+        run('mkdir -p /tmp')
+
+        # Upload the archive to /tmp/ directory on the web server
+        put(archive_path, '/tmp')
+
+        # Uncompress the archive to the specified folder
+        run('tar -xzvf {} -C {}'.format(remote_archive_path, store_path))
+
+        # Remove the uploaded archive from /tmp/
+        run('rm -rf {}'.format(remote_archive_path))
+
+        # Remove the existing symbolic link
+        run('rm -rf /data/web_static/current')
+
+        # Create a new symbolic link
+        run('ln -s {} /data/web_static/current'.format(store_path))
+
+        # Sync contents and remove source files
+        run('rsync -av --remove-source-files {}/web_static/ \
+        /data/web_static/current/'.format(store_path))
+
+        print("Deployment successful!")
         return True
-    except Exception:
+
+    except Exception as e:
+        print(f"Error during deployment: {e}")
         return False
